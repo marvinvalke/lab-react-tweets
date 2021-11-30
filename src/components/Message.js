126dbbca49f1637d8483572693deca1633c2ce71
Marvin Valke <valkemarvin@gmail.com>
2021-11-30 18:05:46 +0100

    lab solved

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+//import React from 'react'
+
+function Message(props) {
+    console.log(props)
+    return (
+        <div>
+            <p className="message"> {props.message} </p>
+
+        </div>
+    )
+}
+
+export default Message
---
+++ src/components/ProfileImage.js
@@ -0,0 +1,12 @@
+//import React from 'react'
+
+function ProfileImage(props) {
+    return (
+        <div>
+            <img src={props.src} className="profile" alt="profile"/>
+
+        </div>
+    )
+}
+
+export default ProfileImage
